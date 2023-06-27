4469749440f1051e5e62d024c7ad0b8a489777d2
jyxop <2000dimitrije@gmail.com>
2023-06-27 23:24:34 +0200

    Kreirana klasa Operacije i definisane vrednosti za svaku od operacija

---
+++ KarateZajednicki/src/main/java/rs/ac/bg/fon/nprog/konstante/Operacije.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.bg.fon.nprog.konstante;
+
+/**
+ *
+ * @author HP
+ */
+public class Operacije {
+    
+    public static final int LOGIN = 1;
+    public static final int VRATI_GRADOVE = 22;
+    public static final int VRATI_GRUPE = 33;
+    public static final int ZAPAMTI_CLANA = 44;
+    public static final int VRATI_CLANOVE = 45;
+    public static final int NADJI_CLANOVE = 46;
+    public static final int UCITAJ_CLANA = 47;
+    public static final int IZMENI_CLANA = 48;
+    public static final int OBRISI_CLANA= 49;
+    public static final int VRATI_TRENINGE =111;
+    public static final int VRATI_SALE = 55;
+    public static final int ZAPAMTI_TRENING=112;
+    public static final int VRATI_VEZBE=66;
+    public static final int NADJI_TRENINGE = 113;
+    public static final int UCITAJ_TRENING = 114;
+    public static final int IZMENI_TRENING=115;
+    public static final int OBRISI_TRENING=116;
+    public static final int IZLOGUJ_TRENERA=120;
+    
+}
