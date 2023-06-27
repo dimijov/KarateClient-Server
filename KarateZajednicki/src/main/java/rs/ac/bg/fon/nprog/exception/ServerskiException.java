6c41ec09ccd223a3f220c2292200ba0d4e3e976d
jyxop <2000dimitrije@gmail.com>
2023-06-27 23:30:33 +0200

    Dodata klasa ServerskiException koja nasledjuje Exception

---
+++ KarateZajednicki/src/main/java/rs/ac/bg/fon/nprog/exception/ServerskiException.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.bg.fon.nprog.exception;
+
+/**
+ *
+ * @author HP
+ */
+public class ServerskiException extends Exception{
+    
+    public ServerskiException(String message) {
+        super(message);
+    }
+    
+}
