8a11410c16b95f128a0d8ac3727a69e1a30b7560
jyxop <2000dimitrije@gmail.com>
2023-06-28 00:45:49 +0200

    Dodati ValidacijaException i KomunikacijaException na klijentskoj strani

---
+++ KarateKlijent/src/main/java/rs/ac/bg/fon/nprog/exception/KomunikacijaException.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.bg.fon.nprog.exception;
+
+/**
+ *
+ * @author HP
+ */
+public class KomunikacijaException extends Exception {
+
+    public KomunikacijaException(String message){
+        super(message);
+    }
+    
+    
+}
---
+++ KarateKlijent/src/main/java/rs/ac/bg/fon/nprog/exception/ValidacijaException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package rs.ac.bg.fon.nprog.exception;
+
+/**
+ *
+ * @author HP
+ */
+public class ValidacijaException extends Exception {
+    
+    public ValidacijaException(String message){
+        super(message);
+    }
+}
